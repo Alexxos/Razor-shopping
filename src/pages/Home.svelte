3859a6a1be9bfdfe02e976d3975e6d119eea5bdc
unknown <alexos.droid@gmail.com>
2020-06-24 10:13:05 +0700

    first commit

---
+++ src/pages/Home.svelte
@@ -0,0 +1,13 @@
+<script>
+import Hero from '../Components/Hero.svelte'
+import {link} from 'svelte-routing'
+// featured products
+import Featured from '../Components/Products/Featured.svelte'
+</script>
+<style>
+
+</style>
+<Hero>
+<a href='/products' class='btn btn-primary btn-hero' use:link>Shop Now</a>
+</Hero>
+<Featured title='featured products'/>
---
+++ src/pages/Products.svelte
@@ -0,0 +1,7 @@
+<script>
+import Products from '../Components/Products/Products.svelte'
+</script>
+
+<h1>Welcome to Product Page</h1>
+
+<Products title='our products' />
